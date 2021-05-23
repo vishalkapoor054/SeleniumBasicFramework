6381a497e4c520a67a31d7f2cdbcaf317b56fe24
deepakautomation <sood07deepak@gmail.com>
2021-05-22 21:26:20 -0400

    added openbrowser code in commonfunctions

--- src/core/OpenAndCloseBrowser.java
+++ src/core/OpenAndCloseBrowser.java
@@ -2,6 +2,7 @@ package core;
 
 import java.util.concurrent.TimeUnit;
 
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
